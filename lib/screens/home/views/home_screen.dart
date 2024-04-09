00654b6f53e876620838eac00397927fa29fd93c
Ramika Perera <119260286+noobramika@users.noreply.github.com>
2024-04-09 17:19:08 +0530

    add expense page, fiebase

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:spendly/screens/add_expense/views/add_expense.dart';
 import 'package:spendly/screens/home/views/main_screen.dart';
 
 import '../../stats/stats.dart';
@@ -53,7 +54,14 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => const AddExpense(),
+                ),
+            );
+          },
         shape: const CircleBorder(),
         child: Container(
           width: 60,
