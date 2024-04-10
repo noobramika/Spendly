b2c7e946945929bbc469e729e99db42c6918db11
Thamashwerago <62113345+Thamashwerago@users.noreply.github.com>
2024-04-10 21:20:22 +0530

    Revert "add expense firebase"
    This reverts commit 738f1066652809541ad2cc636446939c70c55ed6.

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -1,21 +1,8 @@
-import 'package:expense_repository/expense_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
-<<<<<<< Updated upstream
 import 'package:spendly/services/database.dart';
-=======
-<<<<<<< HEAD
-import 'package:spendly/screens/add_expense/blocs/create_categorybloc/create_category_bloc.dart';
-import 'package:uuid/uuid.dart';
-=======
-import 'package:spendly/services/database.dart';
->>>>>>> a86378c00db10f8b3b206893e7070504aa4eedda
->>>>>>> Stashed changes
 
 class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
@@ -103,24 +90,6 @@ class _AddExpenseState extends State<AddExpense> {
                     color: Colors.grey,
                   ),
                   suffixIcon: IconButton(
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (ctx) {
-                            bool isExpended = false;
-                            String iconSelected = '';
-                            Color categoryColor = Colors.white;
-                            TextEditingController categoryNameController = TextEditingController();
-                            TextEditingController categoryIconController = TextEditingController();
-                            TextEditingController categoryColorController = TextEditingController();
-
-                            return StatefulBuilder(
-                              builder: (ctx, setState) {
-=======
->>>>>>> Stashed changes
                       onPressed: () {
                         showDialog(
                             context: context,
@@ -128,10 +97,6 @@ class _AddExpenseState extends State<AddExpense> {
                               bool isExpended = false;
                               return StatefulBuilder(
                                   builder: (context, setState) {
-<<<<<<< Updated upstream
-=======
->>>>>>> a86378c00db10f8b3b206893e7070504aa4eedda
->>>>>>> Stashed changes
                                 return AlertDialog(
                                   title: const Text(
                                     'Create a Category',
@@ -139,52 +104,6 @@ class _AddExpenseState extends State<AddExpense> {
                                         fontWeight: FontWeight.bold,
                                         color: Color(0xFF00B2E7)),
                                   ),
-<<<<<<< HEAD
-                                  content: SizedBox(
-                                    width: MediaQuery.of(context).size.width,
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        TextFormField(
-                                          controller: categoryNameController,
-                                          textAlignVertical: TextAlignVertical.center,
-                                          decoration: InputDecoration(
-                                            isDense: true,
-                                            filled: true,
-                                            fillColor: Colors.white,
-                                            hintText: 'Name',
-                                            border: OutlineInputBorder(
-                                                borderRadius: BorderRadius.circular(12),
-                                                borderSide: BorderSide.none
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(height: 20,),
-                                        TextFormField(
-                                          controller: categoryIconController,
-                                          onTap: () {
-                                            setState(() {
-                                              isExpended = !isExpended;
-                                            });
-                                          },
-                                          textAlignVertical: TextAlignVertical.center,
-                                          readOnly: true,
-                                          decoration: InputDecoration(
-                                            isDense: true,
-                                            filled: true,
-                                            suffixIcon: const Icon(CupertinoIcons.chevron_down, size: 12,),
-                                            fillColor: Colors.white,
-                                            hintText: 'Icon',
-                                            border: OutlineInputBorder(
-                                                borderRadius: isExpended
-                                                ? const BorderRadius.vertical(
-                                                    top: Radius.circular(12)
-                                                  )
-                                                : BorderRadius.circular(12),
-                                                borderSide: BorderSide.none
-                                            ),
-                                          ),
-=======
                                   content: Column(
                                     mainAxisSize: MainAxisSize.min,
                                     children: [
@@ -283,156 +202,9 @@ class _AddExpenseState extends State<AddExpense> {
                                               borderRadius:
                                                   BorderRadius.circular(12),
                                               borderSide: BorderSide.none),
-<<<<<<< Updated upstream
-=======
->>>>>>> a86378c00db10f8b3b206893e7070504aa4eedda
                                         ),
-                                        isExpended
-                                        ? Container(
-                                            width: MediaQuery.of(context).size.width,
-                                            height: 200,
-                                            decoration: const BoxDecoration(
-                                                color: Colors.white,
-                                                borderRadius: BorderRadius.vertical(
-                                                  bottom: Radius.circular(12)
-                                                )
-                                              ),
-                                          child: Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: GridView.builder(
-                                              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                                crossAxisCount: 3,
-                                                mainAxisSpacing: 5,
-                                                crossAxisSpacing: 5
-                                              ),
-                                              itemCount: myCategoriesIcons.length,
-                                                itemBuilder: (context, int i) {
-                                                  return GestureDetector(
-                                                    onTap: () {
-                                                      setState(() {
-                                                        iconSelected = myCategoriesIcons[i];
-                                                      });
-                                                    },
-                                                    child: Container(
-                                                      width: 50,
-                                                      height: 50,
-                                                      decoration: BoxDecoration(
-                                                        border: Border.all(
-                                                          width: 1,
-                                                          color: iconSelected == myCategoriesIcons[i]
-                                                              ? Colors.green
-                                                              : Colors.grey
-                                                        ),
-                                                        borderRadius: BorderRadius.circular(12),
-                                                        image: DecorationImage(
-                                                          image: AssetImage(
-                                                            'assets/${myCategoriesIcons[i]}.png'
-                                                          )
-                                                        )
-                                                      ),
-                                                    ),
-                                                  );
-                                                }
-                                            ),
-                                          ),
-                                        )
-                                        : Container(),
-                                        const SizedBox(height: 20,),
-                                        TextFormField(
-                                          controller: categoryColorController,
-                                          onTap: () {
-                                            showDialog(
-                                                context: context,
-                                                builder: (ctx2) {
-                                                  return BlocProvider.value(
-                                                    value: context.read<CreateCategoryBloc>(),
-                                                    child: AlertDialog(
-                                                      content: Column(
-                                                        mainAxisSize: MainAxisSize.min,
-                                                        children: [
-                                                          ColorPicker(
-                                                            pickerColor: categoryColor,
-                                                            onColorChanged: (value) {
-                                                              setState(() {
-                                                                categoryColor = value;
-                                                              });
-                                                            },
-                                                          ),
-                                                          SizedBox(
-                                                            width: double.infinity,
-                                                            height: 50,
-                                                            child: TextButton(
-                                                                onPressed: () {
-                                                                  Navigator.pop(ctx2);
-                                                                },
-                                                                style: TextButton.styleFrom(
-                                                                    backgroundColor: Colors.black,
-                                                                    shape: RoundedRectangleBorder(
-                                                                        borderRadius: BorderRadius.circular(30)
-                                                                    )
-                                                                ),
-                                                                child: const Text(
-                                                                  'Save',
-                                                                  style: TextStyle(
-                                                                      color: Colors.white,
-                                                                      fontSize: 20
-                                                                  ),
-                                                                )
-                                                            ),
-                                                          )
-                                                        ],
-                                                      ),
-                                                    ),
-                                                  );
-                                                }
-                                            );
-                                          },
-                                          textAlignVertical: TextAlignVertical.center,
-                                          readOnly: true,
-                                          decoration: InputDecoration(
-                                            isDense: true,
-                                            filled: true,
-                                            fillColor: categoryColor,
-                                            hintText: 'Color',
-                                            border: OutlineInputBorder(
-                                                borderRadius: BorderRadius.circular(12),
-                                                borderSide: BorderSide.none
-                                            ),
-                                          ),
->>>>>>> Stashed changes
-                                        ),
-                                        const SizedBox(height: 16,),
-                                        SizedBox(
-                                          width: double.infinity,
-                                          height: kToolbarHeight,
-                                          child: TextButton(
-                                              onPressed: () {
-                                                // Create category object and POP
-                                                Category category = Category.empty;
-                                                category.categoryId = const Uuid().v1();
-                                                category.name = categoryNameController.text;
-                                                category.icon = iconSelected;
-                                                category.color = categoryColor.toString();
-                                                context.read<CreateCategoryBloc>().add(CreateCategory(category));
-                                                // Navigator.pop(context);
-                                              },
-                                              style: TextButton.styleFrom(
-                                                  backgroundColor: Colors.black,
-                                                  shape: RoundedRectangleBorder(
-                                                      borderRadius: BorderRadius.circular(30)
-                                                  )
-                                              ),
-                                              child: const Text(
-                                                'Save',
-                                                style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 20
-                                                ),
-                                              )
-                                          ),
-                                        )
-                                      ],
-                                    ),
+                                      ),
+                                    ],
                                   ),
                                 );
                               });
